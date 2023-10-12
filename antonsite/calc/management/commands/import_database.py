45eea3567dbacdb6e1f2ea23062989dd3e3aa801
Anton Murer <antonmurer.03@gmail.com>
2023-10-12 12:35:31 +0200

    fixed inconsistencies in old database and added missing item pictures

--- antonsite/calc/management/commands/import_database.py
+++ antonsite/calc/management/commands/import_database.py
@@ -37,11 +37,9 @@ class Command(BaseCommand):
 
                     for i in range(0, input_count * 2, 2):
                         item = spliters[i + 3]
-                        print(item)
                         item_readable = self.make_string_readable(spliters[i + 3])
                         amount = spliters[i + 4]
                         item_model: models.ItemModel = models.ItemModel.objects.filter(item_name=item)[0]
-                        print(item_model)
                         input_model = models.InputModel.objects.create(recipe=recipe_model, item=item_model, item_name=item, amount=amount, item_name_readable=item_readable)
 
                     for i in range(0, output_count * 2, 2):
@@ -49,6 +47,5 @@ class Command(BaseCommand):
                         item_readable = self.make_string_readable(spliters[i + (2 * input_count) + 4])
                         amount = spliters[i + (2 * input_count) + 5]
                         item_model: models.ItemModel = models.ItemModel.objects.filter(item_name=item)[0]
-                        print(item_model)
                         output_model = models.OutputModel.objects.create(recipe=recipe_model, item=models.ItemModel.objects.filter(item_name=item)[0], item_name=item, amount=amount, item_name_readable=item_readable)
  
